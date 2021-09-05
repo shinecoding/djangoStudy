99de1150eb40c0369e7c0463269f91b12a2c2009
shinecoding <choiyoungeun@gmail.com>
2021-09-06 00:14:21 +0900

    Users App

--- projects/models.py
+++ projects/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 import uuid
+from users.models import Profile
 
 class Project(models.Model):
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     # blank form 전송할 때 form의 칸이 blank여도 괜찮다

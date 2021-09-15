4f62f9058e3d88d24fcd75550c347d37b83e0eb9
shinecoding <choiyoungeun@gmail.com>
2021-09-15 21:53:06 +0900

    user registration

--- users/views.py
+++ users/views.py
@@ -4,9 +4,12 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
+from .forms import CustomUserCreationForm
+#from django.contrib.auth.forms import UserCreationForm
+
 
 def loginUser(request):
-
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('profiles')
 
@@ -34,6 +37,28 @@ def logoutUser(request):
     logout(request)
     messages.error(request, 'User was successfully logged out!')
     return redirect('login')
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False) #인스턴스 생성
+            user.username = user.username.lower() #소문자변환
+            user.save() #저장
+
+            messages.success(request, 'User account was created')
+
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.success(request, 'An error has occured during registration')
+    context = {'page': page, 'form': form}
+    return render(request, 'users/login_register.html', context)
+
+
 
 def profiles(request):
     profiles = Profile.objects.all()

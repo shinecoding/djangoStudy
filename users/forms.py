0a6933a2ba7ebb4189a671beae0f830e34db4532
shinecoding <choiyoungeun@gmail.com>
2021-09-15 21:59:18 +0900

    user registration

--- users/forms.py
+++ users/forms.py
@@ -7,3 +7,4 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'email', 'username', 'password1', 'password2']
+        labels = {'first_name': 'Name', }

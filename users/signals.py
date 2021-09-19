18510dca8c98687d786fafd912ae62856d932c8b
shinecoding <choiyoungeun@gmail.com>
2021-09-20 01:02:29 +0900

    User Edit Profile
    22

--- users/signals.py
+++ users/signals.py
@@ -22,7 +22,15 @@ def createProfile(sender, instance, created, **kwargs):
     # print('Instance:', instance)
     # print('Created', created)
 
-
+def updateUser(sender, instance, created, **kwargs):
+    profile = instance
+    user = profile.user
+    if created == False:
+        user.first_name = profile.name
+        user.username = profile.username
+        user.email = profile.email
+        user.save()
+        
 def deleteUser(sender, instance, **kwargs):
     user = instance.user
     user.delete()
@@ -30,4 +38,5 @@ def deleteUser(sender, instance, **kwargs):
     print('Deleting user...')
 
 post_save.connect(createProfile, sender=User)
+post_save.connect(updateUser, sender=Profile)
 post_delete.connect(deleteUser, sender=Profile)

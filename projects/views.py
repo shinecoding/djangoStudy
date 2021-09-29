7bfd161b03c552dc7d315c88a625392b333eb5b5
shinecoding <choiyoungeun@gmail.com>
2021-09-29 21:40:21 +0900

    .projects pagination

--- projects/views.py
+++ projects/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.contrib import messages
 from django.db.models import Q
 from .models import Project, Tag
 from .forms import ProjectForm
 from .utils import searchProjects
 
+
+
 def projects(request):
     projects, search_query = searchProjects(request)
 
+    page = 1
+    results = 3
+    paginator = Paginator(projects, results)
+
+    projects = paginator.page(page)
 
     context = { 'projects' : projects, 'search_query': search_query}
     return render(request, 'projects/projects.html', context)

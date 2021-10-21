4369367e22fe82d60de3c5cdc61c185c674bd08f
shinecoding <choiyoungeun@gmail.com>
2021-10-21 22:35:12 +0900

    email

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -121,6 +121,17 @@ USE_L10N = True
 USE_TZ = True
 
 
+#django email 검색
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+#gmail port 검색
+#port for SSL:465, port for TLS/STARTTLS:587
+EMAIL_PORT = 587 
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mbtitest001@gmail.com'
+EMAIL_HOST_PASSWORD = 'dpaqlxldkdl0328'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
